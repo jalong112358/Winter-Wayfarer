e47949047478164369e17d8b9a868197b20a91a0
Jake <jake.long112358@gmail.com>
2019-04-04 17:59:05 -0500

    Clean up ignored files

--- config/keys_dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  email: "jake.long112358@gmail.com",
-
-  clientId:
-    "426085395496-0hu3iukkh0jmspeotilviktcobhjkbn9.apps.googleusercontent.com",
-  clientSecret: "Fy19K5H9hZMMwyuIm6twyvjT",
-  refreshToken: "1/lJASWjCOtCT497P9TaukuIsETA-_bsqe7I1nl6t6fVk"
-};
